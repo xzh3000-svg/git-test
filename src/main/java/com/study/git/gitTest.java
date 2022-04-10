8264cd2645a0527bb8ed62d08c6f2d33265c5874
xzh <xiaozhihui0101118@163.com>
2022-04-10 10:54:03 +0800

    hot-fix 2 commit

--- src/main/java/com/study/git/gitTest.java
+++ src/main/java/com/study/git/gitTest.java
@@ -6,5 +6,7 @@ public class gitTest {
         System.out.println("2");
         System.out.println("3");
         System.out.println("4");
+        System.out.println("5");
+        System.out.println("hot fix");
     }
 }
